ab60ffb6b14a4e1e5816967feba26287783d6f54
arctan2 <prateekbm005@gmail.com>
2025-12-24 18:48:11 +0530

    added types

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+---@param opts { args: string[] | string, type?: "text" }
+---@param bufnr number?
 local function make_scratch(opts, bufnr)
 	bufnr = bufnr or 0
 
@@ -12,17 +14,20 @@ local function make_scratch(opts, bufnr)
 		return
 	end
 
-	if opts.args == "" then return end
-
-	local cmd = opts.args:gsub("^!", "")
-	vim.cmd("0read !" .. cmd)
+	local args = tostring(opts.args)
+	if args == "" then return end
+	args = args:gsub("^!", "")
+	vim.cmd("0read !" .. args)
 end
 
+---@param bufnr number?
 local function is_buf_in_view(bufnr)
 	local win_list = vim.fn.win_findbuf(bufnr)
 	return #win_list > 0
 end
 
+---@param opts { args: string[] | string, type?: "text" }
+---@param bufnr number?
 function M.new_buf(opts, bufnr)
 	if bufnr then
 		vim.api.nvim_set_current_buf(bufnr)
@@ -32,6 +37,8 @@ function M.new_buf(opts, bufnr)
 	make_scratch(opts, bufnr)
 end
 
+---@param opts { args: string[] | string, type?: "text" }
+---@param bufnr number?
 function M.new_vsplit(opts, bufnr)
 	if bufnr ~= nil then
 		if not is_buf_in_view(bufnr) then
@@ -43,6 +50,8 @@ function M.new_vsplit(opts, bufnr)
 	make_scratch(opts, bufnr)
 end
 
+---@param opts { args: string[] | string, type?: "text" }
+---@param bufnr number?
 function M.new_hsplit(opts, bufnr)
 	if bufnr ~= nil then
 		if not is_buf_in_view(bufnr) then
@@ -55,27 +64,22 @@ function M.new_hsplit(opts, bufnr)
 end
 
 function M.new_float(opts, wopts)
-	local bufnr = 0
-	local win = opts.win or 0
+	local bufnr = vim.api.nvim_create_buf(false, true)
 
-	if opts.bufnr == nil then
-		bufnr = vim.api.nvim_create_buf(false, true)
+	local width = math.floor(vim.o.columns * 0.3)
+	local height = math.floor(vim.o.lines * 0.3)
 
-		local width = math.floor(vim.o.columns * 0.3)
-		local height = math.floor(vim.o.lines * 0.3)
+	wopts = wopts or {
+		style = "minimal",
+		relative = "editor",
+		width = width,
+		height = height,
+		row = 0,
+		col = vim.o.columns - width,
+		border = "rounded",
+	}
 
-		wopts = wopts or {
-			style = "minimal",
-			relative = "editor",
-			width = width,
-			height = height,
-			row = 0,
-			col = vim.o.columns - width,
-			border = "rounded",
-		}
-
-		win = vim.api.nvim_open_win(bufnr, true, wopts)
-	end
+	local win = vim.api.nvim_open_win(bufnr, true, wopts)
 
 	make_scratch(opts, bufnr)
 
